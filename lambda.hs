bd1adb21ae5036f8d4d5435a902c38ec9ab678f8
Pentti Sunila <penttisunila@TVT-PENTTIS-MLT.local>
2018-05-23 15:25:23 +0300

    23.5. workshop

--- lambda.hs
+++ lambda.hs
@@ -33,33 +33,45 @@ freeVariables (LambdaVariable a) = [(LambdaVariable a)]
 freeVariables expr@(Abstraction a b) = [y | y <- (freeVariables b), y /= a] -- set literal goodness, copypaste from intructor @ ....
 freeVariables expr@(Application a b) = freeVariables a `union` freeVariables b
 
+bindings :: LambdaTerm -> [LambdaTerm]
+bindings (LambdaVariable a) = []
+bindings expr@(Abstraction a b) = [a] ++ bindings b
+bindings expr@(Application a b) = bindings a ++ bindings b
+
 -- TODO simplify typedef to make sense of dispatch
 variables :: LambdaTerm -> [LambdaTerm]
-variables expr = undefined -- [y | y <- (subTerms expr), ]
+variables (LambdaVariable a) = [(LambdaVariable a)]
+variables expr@(Abstraction a b) = variables a ++ variables b
+variables expr@(Application a b) = variables a ++ variables b
 
 -- TODO
-rename :: LambdaTerm -> LambdaTerm -> LambdaTerm
--- rename a (LambdaVariable b)                  = 
--- rename _ _                                   = undefined
-rename a b = helperfunction a b where
-  frees = freeVariables a
-  helperfunction = undefined
+rename :: LambdaTerm -> LambdaTerm -> LambdaTerm -> LambdaTerm
+rename term var newVar = helperfunction term var newVar where
+  frees = freeVariables term
+  vars  = variables term
+  binds = bindings term 
+  helperfunction  term@(LambdaVariable a1) name newName = if (term == name  && not (elem name binds) && not (elem newName frees) && not (elem newName binds)) then newName else term
+  helperfunction  term@(Abstraction a b) name newName   = (Abstraction a (helperfunction b name newName))
+  helperfunction  term@(Application a b) name newName   = (Application (helperfunction a name newName) (helperfunction b name newName))
+  helperfunction  _ _ _ = undefined
 
 -- check free and bound vars
 alphaCongruent :: LambdaTerm -> LambdaTerm -> Bool
 alphaCongruent = undefined
 
-substitute :: LambdaTerm -> LambdaTerm -> LambdaTerm
-substitute = undefined
-
--- check if abstraction, substitute
+-- performs single step of beta reduction on an application
 betaReduce :: LambdaTerm -> LambdaTerm
-betaReduce = undefined
+betaReduce (Application (Abstraction a b) c) = betaReduce $ rename b a c
+betaReduce a = a
+
+-- TODO recurse beta reduce to reduce whole expressions
+
 
 -- find next unused var name in alphabet, maybe lazy zip of alphabet symbols
+-- 
 alphabet = ['a'..'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9'] 
 genVariable :: [LambdaTerm] -> LambdaTerm
-genVariable = undefined
+genVariable env = undefined
 
 isNormalForm :: LambdaTerm -> Bool
 isNormalForm expr = expr == betaReduce expr
@@ -78,12 +90,13 @@ yCombinator = (Abstraction (LambdaVariable "g") (Application (Abstraction (Lambd
                                                                                            (Application (LambdaVariable "x")
                                                                                                         (LambdaVariable "x"))))))
 printVerbose :: LambdaTerm -> String -> String
-printVerbose expr title = title ++ "\nSubterms\n" ++ unlines (map (show) (subTerms expr)) ++ "\nFree Variables\n" ++ unlines (map (show) (freeVariables expr))
+printVerbose expr title = title ++ "\nSubterms\n" ++ unlines (map (show) (subTerms expr)) ++ "\nVariables\n" ++ unlines(map (show) (variables expr)) ++
+                          "\nFree Variables\n" ++ unlines (map (show) (freeVariables expr)) ++ "\nBindings\n" ++ unlines (map (show) (bindings expr))
+
 --
 main :: IO ()
 main = do
   putStrLn $ printVerbose yCombinator "The Y-combinator"
   putStrLn $ printVerbose example1 "Example term 1"
   putStrLn alphabet
-
-
+  
