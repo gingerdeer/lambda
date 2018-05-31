88f8551c50cc113dc2f356e09ef078f46d81baee
pena <pena@sunicom.fi>
2018-05-31 15:13:42 +0300

    update readme

--- lambda.hs
+++ lambda.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Test.QuickCheck.All
 import Test.QuickCheck
 
 import Text.ParserCombinators.Parsec hiding (spaces)
@@ -7,8 +8,6 @@ import System.Environment
 -- TODO shorthand type synonyms
 -- IDEA use de Bruijn index system internally to avoid problems
 --      with scope and variable naming
--- very simple typedef, with this its for example possible to define an abstraction over a term which is not possible in LC
--- TODO better typedef
 data LambdaTerm = LambdaVariable String |
                   Abstraction LambdaTerm  LambdaTerm |
                   Application LambdaTerm  LambdaTerm
@@ -20,7 +19,30 @@ instance Eq LambdaTerm where
   _ == _ = False
                                                           -- idea: Application a b <alpha> Application c d => terms alpha congruent
 
--- TODO simplifying rules
+
+-- TODO implement λ-> maybe
+-- add type variables and arrow types
+-- IDEA leverage haskells own typesystem??
+-- data lambdaVar a
+--data (Eq a) => LambdaVar a = LVar a
+-- typeVar = String
+-- type = typeVar | Arrow typeVar typeVar
+
+data SimpleType = TypeVar String | Arrow SimpleType SimpleType
+
+instance Show SimpleType where
+  show (Arrow f@(Arrow _ _) c) = "("++ show f ++")" ++ "->" ++ show c
+  show (Arrow a f@(Arrow _ _)) = show a ++ "-> (" ++ show f ++ ")" 
+  show (Arrow (TypeVar a) (TypeVar b)) =  a ++ "->" ++ b
+  show (TypeVar a) = a
+
+exampleType1 :: SimpleType
+exampleType1 = (Arrow (Arrow (TypeVar "A") (TypeVar "B")) (TypeVar "C"))
+
+getType :: LambdaTerm -> SimpleType
+getType = undefined
+
+
 instance Show LambdaTerm where
   show (LambdaVariable a) = a
   show (Abstraction a b)  = "λ" ++ show a ++ "." ++ show b ++ ""
@@ -35,57 +57,52 @@ subTerms expr@(Application a b)  = [expr] ++ subTerms a ++ subTerms b
 -- works
 freeVariables :: LambdaTerm -> [LambdaTerm]
 freeVariables (LambdaVariable a) = [(LambdaVariable a)]
-freeVariables expr@(Abstraction a b) = [y | y <- (freeVariables b), y /= a] -- set literal goodness, copypaste from intructor @ ....
-freeVariables expr@(Application a b) = freeVariables a `union` freeVariables b
+freeVariables expr@(Abstraction a b) = [y | y <- (freeVariables b), y /= a] -- set comprehension copypaste from intructor @ github.com/jllang
+freeVariables expr@(Application a b) = freeVariables a `union` freeVariables b -- sometimes i forget you can just write math in haskell
 
 bindings :: LambdaTerm -> [LambdaTerm]
-bindings (LambdaVariable a) = []
+bindings (LambdaVariable a)     = []
 bindings expr@(Abstraction a b) = [a] ++ bindings b
 bindings expr@(Application a b) = bindings a ++ bindings b
 
--- TODO simplify typedef to make sense of dispatch
 variables :: LambdaTerm -> [LambdaTerm]
-variables (LambdaVariable a) = [(LambdaVariable a)]
+variables (LambdaVariable a)     = [(LambdaVariable a)]
 variables expr@(Abstraction a b) = variables a ++ variables b
 variables expr@(Application a b) = variables a ++ variables b
 
--- TODO
-rename :: LambdaTerm -> LambdaTerm -> LambdaTerm -> LambdaTerm
-rename term var newVar = helperfunction term var newVar where
-  frees = freeVariables term
-  vars  = variables term
-  binds = bindings term 
-  helperfunction  term@(LambdaVariable a1) name newName = if (term == name  && not (elem name binds) && not (elem newName frees) && not (elem newName binds)) then newName else term
-  helperfunction  term@(Abstraction a b) name newName   = (Abstraction a (helperfunction b name newName))
-  helperfunction  term@(Application a b) name newName   = (Application (helperfunction a name newName) (helperfunction b name newName))
-  helperfunction  _ _ _ = undefined
-
--- TODO fix: check free and bound variables in exprs
--- 
+-- TODO fix
 alphaCongruent :: LambdaTerm -> LambdaTerm -> Bool
-alphaCongruent (Application a b)  (Application c d) = a `alphaCongruent` c && b `alphaCongruent` d
-alphaCongruent (Abstraction a b)  (Abstraction c d) = a == c && b `alphaCongruent` d
--- alphaCongruent (Abstraction (LambdaVariable a) (LambdaVariable b)) (Abstraction (LambdaVariable c) (LambdaVariable d)) = 
+alphaCongruent (Application a b)  (Application c d)  = a `alphaCongruent` c && b `alphaCongruent` d
+alphaCongruent (Abstraction a b)  (Abstraction c d)  = a `alphaCongruent` c && b `alphaCongruent` d
 alphaCongruent (LambdaVariable a) (LambdaVariable b) = True
 alphaCongruent _ _ = False
 
+rename :: LambdaTerm -> LambdaTerm -> LambdaTerm -> LambdaTerm
+rename term var newVar = helperfunction term var newVar where
+  frees = freeVariables term
+  vars  = variables     term
+  binds = bindings      term 
+  helperfunction term@(LambdaVariable a1) name newName                  = if (term == name  && not (elem name binds) && not (elem newName vars)) then newName else term
+  helperfunction term@(Abstraction a@(LambdaVariable _) b) name newName = if ((elem name $ frees) && not (elem newName $ variables term)) then (Abstraction a (helperfunction b name newName)) else term
+  helperfunction term@(Application a b) name newName                    = (Application (rename a name newName) (rename b name newName))
+  helperfunction  _ _ _                                                 = undefined
+rename term _ _ = term
 
--- performs single step of beta reduction on an application
 betaReduce :: LambdaTerm -> LambdaTerm
-betaReduce val@(Application (Abstraction a b) c) = betaReduce $ rename b a c
-betaReduce val@(Application a b) = Application (betaReduce a) (betaReduce b)
+betaReduce val@(Application (Abstraction a b) c)  = betaReduce $ rename b a c
+betaReduce val@(Application a b)                  = Application (betaReduce a) (betaReduce b)
 betaReduce val@(Abstraction a (LambdaVariable b)) = val
-betaReduce val@(Abstraction a b) = (Abstraction a (betaReduce b))
+betaReduce val@(Abstraction a b)                  = (Abstraction a (betaReduce b))
 betaReduce a = a
 
+-- do it in IO with välivaiheet
 betaReduceIO :: LambdaTerm -> IO LambdaTerm
 betaReduceIO val@(Application (Abstraction a b) c) = do
   putStrLn $ show val
   betaReduceIO $ rename b a c
 betaReduceIO val@(Application a b) = do
   putStrLn $ show val
-  if isNormalForm val then return val else betaReduceIO $ Application (betaReduce a) (betaReduce b)
-  
+  if isNormalForm val then return val else betaReduceIO $ Application (betaReduce a) (betaReduce b)  
 betaReduceIO val@(Abstraction a (LambdaVariable b)) = do
   putStrLn $ show val
   return val
@@ -94,17 +111,15 @@ betaReduceIO val@(Abstraction a b) = do
   if isNormalForm b then return val else betaReduceIO $ (Abstraction a (betaReduce b))
 betaReduceIO a = do return a
 
-
-
 -- no infinite loops in case of non terminating exprs
 -- doesnt really detect infinite growth in expansions:  betaReduceIOSafe (Application example3 yCombinator)
 recDepth = 10
 maxLen = 200
 
 betaReduceSafe :: Int -> LambdaTerm -> LambdaTerm
-betaReduceSafe num val@(Application (Abstraction a b) c) = if num >= recDepth then val else betaReduceSafe (num+1) $ rename b a c
+betaReduceSafe num val@(Application (Abstraction a@(LambdaVariable _) b) c) = if num >= recDepth then val else betaReduceSafe (num+1) $ rename b a c
 betaReduceSafe num val@(Application a b) = if num >= recDepth then val else Application (betaReduceSafe (num+1) a) (betaReduceSafe (num+1) b)
-betaReduceSafe num val@(Abstraction a (LambdaVariable b)) = if num >= recDepth then val else val
+betaReduceSafe num val@(Abstraction a (LambdaVariable b)) = val -- if num >= recDepth then val else val
 betaReduceSafe num val@(Abstraction a b) = if num >= recDepth then val else (Abstraction a (betaReduceSafe (num+1) b))
 betaReduceSafe num a = a
 
@@ -123,12 +138,11 @@ betaReduceIOSafe num val@(Abstraction a b) = if num >= recDepth then return val 
   if isNormalForm b then return val else betaReduceIOSafe (num+1) (Abstraction a (betaReduce b))
 betaReduceIOSafe num a = do return a
 
--- shorthand synonym
+-- shorthand synonyms
 simplifyIO = betaReduceIOSafe 0
 simplify a = if isNormalForm a then a else simplify $ betaReduceSafe 0 a
 
 -- find next unused var name in alphabet, maybe lazy zip of alphabet symbols
--- 
 alphabet = ['a'..'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9']
 lambdaSymbols = alphabet ++ ['λ',' ','(',')','\\','.']
 genVariable :: [LambdaTerm] -> LambdaTerm
@@ -136,18 +150,6 @@ genVariable env = undefined
 
 isNormalForm :: LambdaTerm -> Bool
 isNormalForm expr = expr == betaReduceSafe 0 expr
-
--- TODO parser
-asd :: Char -> Char -> Bool
-asd a b = elem a alphabet && elem b alphabet
-
-valid str = foldl (&&) True $ map (`elem` lambdaSymbols) str
-alphabetRegex = "[" ++ alphabet ++ "]"
-lambdasRegex = "[" ++ lambdaSymbols ++ "]"
-varRegex = "["++alphabetRegex ++ "].*"
-appRegex = "("++varRegex++" "++varRegex++")"
-absRegex = "λ."++varRegex++"|"++"λ."++appRegex
-regex = varRegex ++ "|" ++ absRegex ++ "|" ++ varRegex
 
 parseVar :: Parser LambdaTerm
 parseVar = do
@@ -206,14 +208,9 @@ readTerm :: String -> LambdaTerm
 readTerm input = case parse parseTerm "haskell" input of
   Left  err -> (LambdaVariable ("parse error" ++ show err))
   Right val -> val
-
--- parse :: String -> LambdaTerm
--- parse str = if valid str then undefined else (LambdaVariable "PARSE ERROR")
--- TODO quickcheck to verify correctness
-
--- TODO examples, numbers and arithmetic implementation
-
--- possibly from type theory book
+  
+-- examples, numbers and arithmetic implementation
+-- possibly from type theory book or wikipedia
 example1 = (Abstraction (LambdaVariable "x") (Abstraction (LambdaVariable "y") (Application (Application (LambdaVariable "z") (LambdaVariable"z")) (LambdaVariable "x"))))
 example2 = (Abstraction (LambdaVariable "x") (Application (Abstraction (LambdaVariable "y") (LambdaVariable "y")) (LambdaVariable "x")))
 -- from type theory book
@@ -228,6 +225,9 @@ example4 = (Abstraction (LambdaVariable "f")
                                                (Application (LambdaVariable "f") (LambdaVariable "x"))))))
 -- non reducing
 example5 = (Application (Abstraction (LambdaVariable "x") (Application (LambdaVariable "x") (LambdaVariable "x"))) (Abstraction (LambdaVariable "x") (Application (LambdaVariable "x") (LambdaVariable "x"))))
+
+
+
 
 -- TODO maybe add stackexchange code golf examples
 t1 = readTerm "((λ x. x) (λ y. (λ z. z)))"
@@ -262,8 +262,7 @@ yCombinator = (Abstraction (LambdaVariable "g") (Application (Abstraction (Lambd
 showSubterms :: LambdaTerm -> String
 showSubterms expr = "Subterms\n"++ unlines (map (show) (subTerms expr)) ++ "\n"
 
-
-showVars     expr = "Variables" ++ unlines(map (show) (variables expr)) ++
+showVars     expr = "Variables\n" ++ unlines(map (show) (variables expr)) ++
                     "\nFree Variables\n" ++ unlines (map (show) (freeVariables expr)) ++ "\nBindings\n" ++ unlines (map (show) (bindings expr))
 showSimplify expr = "Reduced form\n" ++ show (betaReduce expr)
 printVerbose :: LambdaTerm -> String -> String
@@ -272,18 +271,84 @@ printVerbose expr title = title ++ "\nSubterms\n" ++ unlines (map (show) (subTer
 
 prettyPrint expr title = title ++ showSubterms expr ++ showVars expr ++ showSimplify expr
 
---
+-- TODO QuickCheck
+-- instance Arbitrary LambdaTerm
+genSafeChar :: Gen Char
+genSafeChar = elements alphabet
+
+genSafeString :: Gen String
+genSafeString = listOf1 genSafeChar
+
+  
+instance Arbitrary LambdaTerm where
+  arbitrary = do
+    n <- choose (1,3) :: Gen Int
+    case n of
+      1 -> do x <- genSafeString :: Gen String
+              let c = take 1 x   :: String
+              return (LambdaVariable c)-- term
+      2 -> do
+        x <- genSafeString :: Gen String
+        let c = take 1 x   :: String
+        y <- arbitrary     :: Gen LambdaTerm
+        return (Abstraction (LambdaVariable c) y)-- abs
+      3 -> do
+        x <- arbitrary :: Gen LambdaTerm
+        y <- arbitrary :: Gen LambdaTerm
+        return (Application x y)-- app
 
 
+-- property tests
+prop_read :: LambdaTerm -> Bool
+prop_read x = x == (readTerm $ show x)
+
+-- example calculations
+-- combinatory logic: simplify (Application (Application sComb kComb) kComb) -> id
+prop_comb = simplify (Application (Application sComb kComb) kComb) `alphaCongruent` iComb
+
+test_t1  = simplify t1 `alphaCongruent` readTerm "(λ y. (λ z. z))"
+test_t2  = simplify t2 `alphaCongruent` readTerm "λ x. x"
+test_t3  = simplify t3 `alphaCongruent` readTerm "(λ y. (λ a. a))"
+test_t4  = simplify t4 `alphaCongruent` readTerm "(λ a. a)"
+test_t5  = simplify t5 `alphaCongruent` readTerm "(λ y. y)"
+test_t6  = simplify t6 `alphaCongruent` readTerm "(λ b. b)"
+test_t7  = simplify t5 `alphaCongruent` readTerm "(λ y. y)"
+test_t8  = simplify t8 `alphaCongruent` readTerm "(λ a. a)"
+test_t9  = simplify t9 `alphaCongruent` readTerm "(λ a. (λ b. (a (a (a (a (a (a (a (a b))))))))))"
 
 
+-- -- options = TestOptions
+-- --       {
+-- --         no_of_tests     = 200
+-- --       , lenght_of_tests = 1
+-- --       , debug_tests     = False
+-- --       }
 
 main :: IO ()
 main = do
+  -- putStrLn "Untyped Lambda Calculus"
+  -- putStrLn "Running tests"
+  -- runTests "simple" options
+  --   [
+  --     run prop_read
+  --   , run prop_comb
+  --   , run test_t1
+  --   , run test_t2
+  --   , run test_t3
+  --   , run test_t4
+  --   , run test_t5
+  --   , run test_t6
+  --   , run test_t7
+  --   , run test_t8
+  --   , run test_t9
+
+  --   ]
   -- putStrLn $ printVerbose yCombinator "The Y-combinator"
   -- putStrLn $ printVerbose example1 "Example term 1"
   -- putStrLn alphabet
   x <- getLine
+ -- y <- arbitrary :: Gen String
+ -- putStrLn y
   if x == "" then return () else do
     putStrLn $ show $ simplify $ readTerm x
     main
